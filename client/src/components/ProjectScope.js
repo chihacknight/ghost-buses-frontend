eff9c817d87fbd3d0e031f3570d9ce76364f9c54
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-07-29 14:19:04 -0500

    Initialize Frontend
    - initialize React app
    - initialize Leaflet mapping
    - includes dummy text and potential uses for CSS animation

---
+++ client/src/components/ProjectScope.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import DetailContainer from './DetailContainer'
+import DotNav from './DotNav'
+
+export default function ProjectScope() {
+    const [detailNavSelect, setDetailNavSelect] = useState('matters')
+
+  return (
+    <div className='project-scope-container padding-container'>
+        <aside>
+            <DotNav setDetailNavSelect={setDetailNavSelect} detailNavSelect={detailNavSelect}/>
+        </aside>
+        <main>
+          <DetailContainer selected={detailNavSelect}/>
+        </main>
+    </div>
+  )
+}
