dca9e9de5923af1e8f5d536af70adf1d4009eece
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-08-24 10:15:47 -0500

    Feat - Add Demo Mode
    - Created Demo state to show map functionality with smaller data set
    - Added example data
    - Updated Map to reflect only routes for which there is demo data.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 
-import mapRoutes from '../Routes/bus_route_shapes_simplified_linestring.json';
+import mapRoutes from "../Routes/bus_route_shapes_simplified_linestring.json";
+import resultsData from "../Routes/dummy_data.json";
 
 export default function Map() {
+  const [demo, setDemo] = useState(false);
+  const [selectedRoute, setSelectedRoute] = useState();
+  const availableRoutes = resultsData.features
+    .map((route) => route.route_id)
+    .filter((v, i, a) => a.indexOf(v) === i);
 
+  const demoMapRoutes = mapRoutes.features.filter((route) =>
+    availableRoutes.includes(route.properties.route_id)
+  );
 
-    function onEachFeature(feature, layer) {
-        if (feature.properties) {
-          const { route_long_name, route_id } = feature.properties;
-          layer.bindTooltip(`${route_id}, ${route_long_name}`, {
-            sticky: true
-          });
-        }
-      }
+  function onEachFeature(feature, layer) {
+    if (feature.properties) {
+      const { route_long_name, route_id } = feature.properties;
+      layer.bindTooltip(`${route_id}, ${route_long_name}`, {
+        sticky: true,
+      });
+    }
+  }
 
   return (
     <div className="map padding-container">
-      <h2 >Map/Data</h2>
+      <h2>Map/Data</h2>
+      <label>
+        <input
+          type="checkbox"
+          checked={demo}
+          onChange={() => setDemo((prevDemo) => !prevDemo)}
+        />
+        Demo
+      </label>
       <MapContainer
         center={[41.881832, -87.623177]}
         zoom={11}
@@ -27,7 +44,11 @@ export default function Map() {
           attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
           url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
         />
-        <GeoJSON data={mapRoutes} onEachFeature={onEachFeature} />
+        <GeoJSON
+          data={demo ? demoMapRoutes : mapRoutes}
+          onEachFeature={onEachFeature}
+          key={demo}
+        />
       </MapContainer>
     </div>
   );
