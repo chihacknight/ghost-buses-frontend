6fb58191dbe8144351d5af2bc3408e81fd329e6f
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-09-15 22:41:32 -0500

    Fixes #7 and Fixes #8

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import "./css/main.css";
-import Map from "./components/Map";
+import { Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header";
-import ProjectScope from "./components/ProjectScope";
-import Intro from "./components/Intro";
 import Footer from "./components/Footer";
 import Socials from "./components/Socials";
+import Nav from "./components/Nav";
 
-function App() {
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import InteractiveMap from "./Pages/InteractiveMap";
+import FurtherReading from "./Pages/FurtherReading";
+import Methods from "./Pages/Methods";
+import HowToHelp from "./Pages/Methods";
+
+const App = () => {
   return (
     <div className="App">
+      <Nav />
       <div className="container">
         <Socials />
         <Header />
-        <Intro />
-        <hr />
-        <ProjectScope />
-        <hr />
-        <Map />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/map" element={<InteractiveMap />} />
+          <Route path="/further-reading" element={<FurtherReading />} />
+          <Route path="/methods" element={<Methods />} />
+          <Route path="/how-to-help" element={<HowToHelp />} />
+        </Routes>
       </div>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Pages/InteractiveMap.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Map from "../components/Map";
+
+const InteractiveMap = () => {
+  return (
+    <div className="page-container">
+      <Map />
+    </div>
+  );
+};
+
+export default InteractiveMap;
