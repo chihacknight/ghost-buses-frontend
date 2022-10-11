4d06eca4e12946153cf794f5c9c85a1a13678bad
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-10-11 15:05:53 -0500

    Fix #10 #13 #14
    - Added existing page copy to respective pages.
    - Fixed import bug in App

--- src/Pages/Methods.js
+++ src/Pages/Methods.js
@@ -1,7 +1,51 @@
 import React from "react";
 
 const Methods = () => {
-  return <div className="page-container">Methods</div>;
+  return (
+    <div className="page-container">
+      <h2>Methods</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque blandit
+        diam quis dui hendrerit, eu viverra massa ultrices. Nam vestibulum at
+        leo eu pellentesque. Fusce fermentum blandit ligula, at faucibus orci
+        dignissim finibus. Integer luctus pulvinar augue ut imperdiet. In
+        ultricies odio et nibh convallis, id posuere sapien ultricies. Duis
+        feugiat felis sed tincidunt dignissim. Proin risus neque, cursus quis
+        sollicitudin sed, semper mattis risus. Quisque ultricies lectus mauris,
+        mattis pulvinar eros luctus id. Maecenas sollicitudin, nisl sed
+        efficitur lobortis, erat eros tempor nisi, nec ultrices nisl odio ut
+        est. Cras ullamcorper, erat in egestas molestie, nisi justo ultrices
+        orci, at sodales ante ligula ac ex. Suspendisse vitae sem congue elit
+        scelerisque venenatis ac ac felis.
+      </p>
+      <p>
+        Proin feugiat, sapien a semper posuere, purus risus efficitur diam, vel
+        euismod ante mi ac ante. Cras nec metus accumsan, sodales lectus non,
+        eleifend elit. Donec eu ante eget nisi efficitur vestibulum. Mauris
+        sodales ligula sed sem imperdiet, sit amet varius odio imperdiet.
+        Maecenas commodo leo at porta viverra. Praesent volutpat interdum
+        libero. Nunc at eros fringilla, ullamcorper lorem vitae, varius ante. Ut
+        sed lacus sed augue vestibulum euismod eu consectetur lacus. Fusce
+        eleifend pulvinar euismod. Nulla rutrum neque et nulla suscipit, vitae
+        vehicula urna faucibus. Praesent id pharetra nisi. Nam sed velit libero.
+        Aliquam neque neque, aliquam at nisi nec, scelerisque pellentesque enim.
+        Fusce dui ante, sollicitudin vel tempor quis, vulputate non massa.
+        Aliquam ultrices libero et odio auctor ultrices.
+      </p>
+      <p>
+        Suspendisse gravida et mi eu condimentum. Aenean sem leo, vestibulum ut
+        erat nec, fermentum elementum sapien. Mauris viverra nisi nec urna
+        luctus, elementum laoreet nulla ultricies. Curabitur tempor ligula eget
+        imperdiet consequat. Integer posuere elit enim, sed finibus tellus
+        tincidunt in. Mauris malesuada tristique augue, aliquet gravida tellus
+        semper sit amet. Praesent eleifend, nunc in faucibus cursus, diam arcu
+        hendrerit enim, nec maximus lectus nisl non purus. Donec euismod turpis
+        non diam imperdiet volutpat. Maecenas congue commodo cursus. Nunc
+        egestas porttitor aliquet. Ut in ullamcorper urna.
+      </p>
+    </div>
+  );
 };
 
 export default Methods;
