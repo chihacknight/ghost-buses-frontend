5984596852bbd8257ce05aede9c317a69d671f25
Sean MacMullan <sean.macmullan@gmail.com>
2023-03-21 19:12:09 -0500

    Implement BusRouteDetails in RouteInfo page.

--- src/Pages/RouteInfo.js
+++ src/Pages/RouteInfo.js
@@ -1,14 +1,10 @@
 import resultsData from "../Routes/data.json";
 
 import React from "react";
-import Modal from "../components/Modal";
+import BusRouteDetails from "../components/BusRouteDetails";
 import { useParams } from "react-router-dom";
 
-
-const closeModal = () => {
-  document.body.style.overflow = "scroll";
-};
-
+//customized; goes off route id
 function findDataForRoute(route) {
   const results = resultsData.features.filter(
     (data) =>
@@ -17,16 +13,57 @@ function findDataForRoute(route) {
   return results;
 }
 
+//copy-pasted from modal component; need to refactor
+const calcBusFraction = (acc) => {
+  let fraction
+  if (acc > 22 && acc < 27) {
+    fraction = [3, 4]
+  } else if (acc > 72 && acc < 77) {
+    fraction = [1, 4]
+  } else {
+    fraction = reduce((100 - round10(Math.round(acc))), 100);
+  }
+
+  return fraction
+}
+
+function round10(x) {
+  let digits = x.toString().split("");
+  if (digits[1] >= 5) {
+    digits = [Number(digits[0]) + 1, 0];
+  } else {
+    digits[1] = 0;
+  }
+  digits = digits.map((x) => Number(x));
+  return parseInt(digits.join(""));
+}
+
+function reduce(numerator, denominator) {
+  var gcd = function gcd(a, b) {
+    return b ? gcd(b, a % b) : a;
+  };
+  gcd = gcd(numerator, denominator);
+  return [numerator / gcd, denominator / gcd];
+}
+
+
+
 const RouteInfo = () => {
 
   let { route } = useParams();
 
+  let selectedRoute = findDataForRoute(route)
+  const totalAcc = (selectedRoute[0].properties.ratio) * 100;
+  const busFraction = calcBusFraction(totalAcc.toFixed(0))
+
   return (
     <div className="page-container">
-      <h1>{route}</h1>
-      
-      {/* <Modal selectedRoute={route} closeModal={closeModal} /> */}
 
+      <BusRouteDetails
+        selectedRoute={selectedRoute}
+        totalAcc={totalAcc}
+        busFraction={busFraction}
+      />
     </div>
   );
 };
