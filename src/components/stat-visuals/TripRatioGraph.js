76b77198563ea9fb89db1e3324c45a0f37fd53ab
Sean MacMullan <sean.macmullan@gmail.com>
2023-03-31 20:32:57 -0500

    Add annotations to graphs. Add graph prop data into constants.

--- src/components/stat-visuals/TripRatioGraph.js
+++ src/components/stat-visuals/TripRatioGraph.js
@@ -11,69 +11,115 @@ function TripRatioGraph({ route_id }) {
     var tripsScheduled = routeTripData.map(datapoint => datapoint.trip_count_sched);
     var ratio = routeTripData.map(datapoint => datapoint.ratio);
 
+    const data_trips = [
+        {
+            mode: "lines",
+            name: "Actual Trips",
+            x: timestamps,
+            xaxis: "x",
+            y: tripsActual,
+            yaxis: "y",
+            type: "scatter"
+        },
+        {
+            mode: "lines",
+            name: "Scheduled Trips",
+            x: timestamps,
+            xaxis: "x",
+            y: tripsScheduled,
+            yaxis: "y",
+            type: "scatter"
+        },
+    ];
+
+    const data_ratio = [
+        {
+            mode: "lines",
+            name: "Ratio",
+            x: timestamps,
+            xaxis: "x",
+            y: ratio,
+            yaxis: "y",
+            type: "scatter",
+        },
+    ];
+
+    const layout_trips = {
+        responsive: true,
+        autosize: true,
+        title: 'Actual vs. Scheduled Trips',
+        yaxis: {
+            title: "Trips"
+        },
+        annotations: [
+            {
+                x: "2022-11-01",
+                y: 20,
+                xref: "x",
+                yref: "y",
+                showarrow: false,
+                text: "CTA announced schedule change<br>January 8, 2023",
+            }
+        ],
+        shapes: [
+            {
+                line: { color: "black", dash: "dot", width: 0.8 },
+                type: "line",
+                x0: "2023-01-08",
+                x1: "2023-01-08",
+                xref: "x",
+                y0: 0,
+                y1: 1,
+                yref: "y domain",
+            }
+        ]
+    };
+
+    const layout_ratio = {
+        responsive: true,
+        autosize: true,
+        title: 'Schedule Attainment',
+        yaxis: {
+            title: 'Ratio of Actual vs. Scheduled Trips',
+            range: [0.0, 1.2],
+            zeroline: false
+        },
+        annotations: [
+            {
+                x: "2022-11-01",
+                y: 0.3,
+                xref: "x",
+                yref: "y",
+                showarrow: false,
+                text: "CTA announced schedule change<br>January 8, 2023",
+            }
+        ],
+        shapes: [
+            {
+                line: { color: "black", dash: "dot", width: 0.8 },
+                type: "line",
+                x0: "2023-01-08",
+                x1: "2023-01-08",
+                xref: "x",
+                y0: 0,
+                y1: 1,
+                yref: "y domain",
+            }
+        ]
+    };
 
     return (
         <div className="trip-performance-graph">
             <Plot
-                data={[
-                    {
-                        mode: "lines",
-                        name: "Actual Trips",
-                        x: timestamps,
-                        xaxis: "x",
-                        y: tripsActual,
-                        yaxis: "y",
-                        type: "scatter",
-                    },
-                    {
-                        mode: "lines",
-                        name: "Scheduled Trips",
-                        x: timestamps,
-                        xaxis: "x",
-                        y: tripsScheduled,
-                        yaxis: "y",
-                        type: "scatter",
-                    },
-                ]}
-                layout={{
-                    width: 800,
-                    height: 400,
-                    title: 'Actual vs. Scheduled Trips',
-                    annotations: {
-                        showarrow: false,
-                        text: "CTA announced schedule change<br>January 8, 2023",
-                        x: "2022-11-01",
-                        xref: "x",
-                        y: 0.3,
-                        yref: "y",
-                    },
-                }}
+                data={data_ratio}
+                layout={layout_ratio}
+                config={{ displayModeBar: false }}
             />
+            
             <Plot
-                data={[
-                    {
-                        mode: "lines",
-                        name: "Ratio",
-                        x: timestamps,
-                        xaxis: "x",
-                        y: ratio,
-                        yaxis: "y",
-                        type: "scatter",
-                    },
-                ]}
-                layout={{
-                    width: 800,
-                    height: 400,
-                    title: 'Ratio of Actual vs. Scheduled',
-                    annotations: {
-                        showarrow: false,
-                        text: "CTA announced schedule change<br>January 8, 2023",
-                        x: "2022-11-01",
-                        xref: "x",
-                        y: 0.3,
-                        yref: "y",
-                    },
-                }}
+                data={data_trips}
+                layout={layout_trips}
+                config={{displayModeBar: false}}
             />
         </div>
     );
