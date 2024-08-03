e97bb4fdb0e06f37ae455993b552c9d3ffde4463
ghosthacker729 <jacquelineostrowski@hotmail.com>
2024-08-02 20:39:59 -0500

    Update FurtherReading.js

--- src/Pages/FurtherReading.js
+++ src/Pages/FurtherReading.js
@@ -5,11 +5,27 @@ const FurtherReading = () => {
     <div className="page-container further-reading">
       <h2>Further Reading</h2>
       <p>
-        Ghost Buses have been written about in local Chicago news. Here is a
-        selection of recent news media articles for further reading.
+       The ghost bus challenge continues to receive regular coverage in Chicago local news. Here’s a selection of recent articles for further reading.
       </p>
       <ul>
-        <li>
+        <li><ul>
+<b>2024</b><br>
+<li><a href="https://chicagoreader.com/news-politics/cta-ridership-recovery/" target="blank">“The CTA’s missing riders” - Chicago Reader,</a> 7/4/24</li>
+<li><a href="https://www.facebook.com/thecta/videos/want-to-know-whats-up-with-ghost-buses-and-trains-youre-not-alone-in-our-first-i/1612666616134566/" target="blank">“Want to know what’s up with ‘ghost’ buses and trains? You’re not alone.”</a> - Ask CTA at Chicago Transit Authority, 3/20/24</li>
+<li><a href="https://www.wbez.org/reset-with-sasha-ann-simons/2024/01/17/chicago-has-a-plan-to-improve-cta-buses-but-some-advocates-want-more" target="blank">“Chicago has a plan to improve CTA buses, but some advocates want more”</a> - WBEZ Chicago, 1/17/24</li>
+</ul>
+<ul>
+<b>2023</b><br>
+<li><a href="https://abc7chicago.com/cta-bus-tracker-app-estimated-arrival-times-chicago/13995684/" target="blank">“CTA bus tracker 'ghosts' are frequent issue for riders, agency says it's caused by staffing shortage”</a> - ABC7 Chicago, 10/31/23</li>
+<li><a href="https://www.wbez.org/politics/2023/10/25/is-the-chicago-cta-reliable-what-riders-say-in-2023" target="blank">“Is CTA getting better? We asked hundreds of riders”</a> - WBEZ Chicago, 10/25/23</li>
+<li><a href="https://www.wbez.org/politics/2023/04/18/chicago-public-transit-cta-tracker-accuracy" target="blank">“CTA riders ask: Why is Chicago’s transit tracker unreliable?”</a> - WBEZ Chicago, 4/18/23</li>
+<li><a href="https://stateline.org/2023/01/31/ghost-buses-haunt-transit-agencies-and-frustrate-riders/" target="blank">“‘Ghost buses’ haunt transit agencies and frustrate riders”</a> - Stateline.org, 1/31/23</li>
+<li><a href="https://blockclubchicago.org/2023/01/12/cta-rolls-back-rapid-transit-for-9-5-riders-in-new-schedules-aimed-to-stop-ghost-buses/" target="blank">“Rush hour riders face longer waits as CTA aims to stop ghost buses, but burned-out operators nervous about change”</a> - Block Club Chicago, 1/12/23</li>
+<li><a href="https://www.nbcchicago.com/news/local/cta-makes-more-changes-to-tackle-ghost-bus-problem/3043139/" target="blank">“CTA makes more changes to tackle ‘ghost bus' problem”</a> - NBC5 Chicago, 1/11/23</li>
+</ul>
+
+<ul>
+<b>2022</b><br>
           <a
             href="https://chicago.suntimes.com/2022/9/14/23351803/kam-buckner-lightfoot-transportation-plan-ghost-buses-trains-cta-chicago-mayor-election-2023"
             target="_blank"
