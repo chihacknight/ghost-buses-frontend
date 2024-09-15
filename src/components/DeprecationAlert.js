c285623e849154b0329494a99d2b9654bbd78a03
laurie merrell <lauriemerrell@gmail.com>
2024-09-15 13:04:15 -0500

    update wording

--- src/components/DeprecationAlert.js
+++ src/components/DeprecationAlert.js
@@ -5,7 +5,7 @@ export default function DeprecationAlert() {
   <div className = "deprecation-alert">
     <p>
     As of September 2024, this site is no longer being updated. <br></br>
-    Our raw data is still available for independent analysis. <br></br>
+    The raw data behind the project is still available for independent analysis. <br></br>
     See <a href='https://github.com/chihacknight/chn-ghost-buses/tree/main/data_analysis#readme'>this GitHub page</a> for information on how to access the raw data.
     </p>
   </div>
